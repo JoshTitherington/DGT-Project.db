720aa839b46c499b103bb9dfd310f48c62003f2f
JoshTitherington07 <211069@trinity.schools.nz>
2024-09-05 11:37:40 +1200

    Lots

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def home():
 
 @app.route("/games")
 def games():
-    return render_template("Page1.html")
+    print("I'm Here", id)
+    conn = sqlite3.connect("Database.db.db")
+    cur = conn.cursor()
+    #cur.execute("SELECT * FROM game WHERE id=?;",(id,))
+    cur.execute(" select * from game order by name;")
+    games = cur.fetchall()
+    conn.close()
+    return render_template("page1.html", games=games)
 
 @app.route("/about")
 def about():
@@ -19,9 +26,38 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/videogame")
-def videogame():
-    return render_template("videogame.html")
+@app.route("/videogame/<int:id>")
+def videogame(id):
+    print("I'm Here", id)
+    conn = sqlite3.connect("Database.db.db")
+    cur = conn.cursor()
+    #cur.execute("SELECT * FROM game WHERE id=?;",(id,))
+    cur.execute(" select game.id, game.name, imgroute.path, imgroute.alt_text from game join imgroute on game.img_id = imgroute.id where game.id = ?;",(id,))
+    game = cur.fetchone()
+    conn.close()
+    return render_template("page.html", game=game)
+
+def main():
+    # Create a list of page data
+    pages = [
+        {'title': 'Page 1', 'image': 'images/page1.jpg', 'alt': 'Page 1 Image', 'description': 'Description for page 1'},
+        {'title': 'Page 2', 'image': 'images/page2.jpg', 'alt': 'Page 2 Image', 'description': 'Description for page 2'},
+        # Add more pages as needed
+    ]
+    return render_template('page1.html', pages=pages)
+
+@app.route('/page/<int:page_id>')
+def page(page_id):
+    pages = [
+        {'title': 'Page 1', 'image': 'images/page1.jpg', 'alt': 'Page 1 Image', 'description': 'Description for page 1'},
+        {'title': 'Page 2', 'image': 'images/page2.jpg', 'alt': 'Page 2 Image', 'description': 'Description for page 2'},
+        # Add more pages as needed
+    ]
+    page = pages[page_id - 1]  # Get the page data based on the ID
+    return render_template('page.html', page_title=page['title'], image_path=page['image'], image_alt=page['alt'], description=page['description'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run()
